9b441c98e3ed1134c0704a25ce5b7443eb4b2c0a
David Requeno <davidrequeno52@gmail.com>
2023-09-07 22:00:34 -0600

    submenu

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -25,7 +25,7 @@ export default function MyNavbar() {
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link color="foreground" href="/topmovies">
             Peliculas populares
           </Link>
         </NavbarItem>
---
+++ src/app/topmovies/submenu.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Divider } from "@nextui-org/react";
+import Link from "next/link";
+
+export default function SubMenu() {
+  return (
+    <div className="max-w-md mx-auto flex justify-center">
+      <div>
+        <Divider className="my-4" />
+        <div className="flex h-5 items-center space-x-4 text-small">
+          <Link href="/">Inicio</Link>
+          <Divider orientation="vertical" />
+          <Link href="/topmovies">Top movies</Link>
+        </div>
+      </div>
+    </div>
+  );
+}

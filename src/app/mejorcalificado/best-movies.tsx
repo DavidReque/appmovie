ed7cc99135fbf690f6744bc602f47dd0324c9f49
David Requeno <davidrequeno52@gmail.com>
2023-09-09 19:13:43 -0600

    submenu mejorcalificado

---
+++ src/app/mejorcalificado/best-movies.tsx
@@ -0,0 +1,9 @@
+import SubMenu from "./submenu";
+
+export default function BestMovie() {
+  return (
+    <div>
+        <SubMenu/>
+    </div>      
+  )
+}
--- src/app/mejorcalificado/page.tsx
+++ src/app/mejorcalificado/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import BestMovie from './best-movies'
 
 export default function Home() {
   return (
-    <div>page</div>
+    <div>
+      <BestMovie/>
+    </div>
   )
 }

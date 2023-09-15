b8f85ec28d0e2557d533eb74811d0560c489d86d
David Requeno <davidrequeno52@gmail.com>
2023-09-14 21:21:50 -0600

    4 movies

--- src/app/components/top-photos.tsx
+++ src/app/components/top-photos.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function TopPhotos() {
 
-  const imageArray = images.slice(0, 5);
+  const imageArray = images.slice(0, 4);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-9">

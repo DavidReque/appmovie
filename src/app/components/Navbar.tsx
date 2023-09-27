9072356870d30aa514ff3d278f7bc527fafe6f51
David Requeno <davidrequeno52@gmail.com>
2023-09-26 23:05:44 -0600

    bg

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarContent, NavbarItem, Link, NavbarMenu, NavbarMenuItem } from '@nextui-org/react'
-import Search from './Search'
+import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarContent, NavbarItem, Link, NavbarMenu, NavbarMenuItem, Input } from '@nextui-org/react'
+import SearchIcon from './SearchIcon'
 
 export default function MyNavbar () {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false)
@@ -41,7 +41,24 @@ export default function MyNavbar () {
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <Search/>
+      <NavbarContent as="div" className="items-center" justify="end">
+        <Input
+        style={{
+          border: 'none',
+          boxShadow: 'none'
+        }}
+          classNames={{
+            base: 'max-w-full sm:max-w-[13rem] h-10',
+            mainWrapper: 'h-full',
+            input: 'text-small',
+            inputWrapper: 'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20'
+          }}
+          placeholder="Buscar..."
+          size="md"
+          startContent={<SearchIcon />}
+          type="search"
+        />
+      </NavbarContent>
         {/* <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
             Sign Up

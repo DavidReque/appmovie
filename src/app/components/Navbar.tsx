0647571b5c43d861d9622ecd3c15a05d151c60a8
David Requeno <davidrequeno52@gmail.com>
2023-09-27 21:39:19 -0600

    overflow

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState, useEffect } from 'react'
-import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarContent, NavbarItem, Link, NavbarMenu, NavbarMenuItem, Input } from '@nextui-org/react'
+import { Navbar, NavbarBrand, NavbarMenuToggle, NavbarContent, NavbarItem, Link, NavbarMenu, NavbarMenuItem, Input, Card, CardHeader, CardBody, Image, Spinner } from '@nextui-org/react'
 import SearchIcon from './SearchIcon'
 import { type Movie } from '../types'
 import { useRouter } from 'next/navigation'
@@ -17,7 +17,7 @@ export default function MyNavbar () {
   const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&query=${query}`
 
   useEffect(() => {
-    if (query) {
+    if (query !== '') {
       setLoading(true)
 
       // Realiza la solicitud a la API de TMDb para buscar películas
@@ -31,7 +31,7 @@ export default function MyNavbar () {
 
           const data = await response.json()
 
-          if (data.results) {
+          if (data.results !== undefined) {
             setSuggestions(data.results)
           } else {
             setSuggestions([])
@@ -65,7 +65,7 @@ export default function MyNavbar () {
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link color="foreground" href="/topmovies">
-            Peliculas populares
+            Películas populares
           </Link>
         </NavbarItem>
         <NavbarItem>
@@ -85,75 +85,94 @@ export default function MyNavbar () {
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-      <NavbarContent as="div" className="items-center" justify="end">
-        <Input
-        style={{
-          border: 'none',
-          boxShadow: 'none'
-        }}
-          classNames={{
-            base: 'max-w-full sm:max-w-[13rem] h-10',
-            mainWrapper: 'h-full',
-            input: 'text-small',
-            inputWrapper: 'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20'
+        <NavbarContent as="div" className="items-center" justify="end">
+          <div style={{ position: 'relative' }}>
+            <Input
+              style={{
+                border: 'none',
+                boxShadow: 'none'
+              }}
+              classNames={{
+                base: 'max-w-full sm:max-w-[13rem] h-10',
+                mainWrapper: 'h-full',
+                input: 'text-small',
+                inputWrapper:
+                  'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20'
+              }}
+              onChange={(e) => {
+                setQuery(e.target.value)
+              }}
+              value={query}
+              placeholder="Buscar..."
+              size="md"
+              startContent={<SearchIcon />}
+              type="search"
+            />
+{suggestions.length > 0 && (
+  <div style={{
+    position: 'absolute',
+    top: '100%',
+    left: 0,
+    width: '100%',
+    maxHeight: '900px',
+    overflowY: 'auto'
+  }}>
+    <Card className="py-4">
+      {loading && (
+        <div className="text-center py-4">
+          <Spinner color="warning" />
+        </div>
+      )}
+      {suggestions.map((movie) => (
+        <li
+          key={movie.id}
+          onClick={() => {
+            router.push(`/${movie.id}`)
           }}
-          onChange={(e) => { setQuery(e.target.value) }}
-          value={query}
-          placeholder="Buscar..."
-          size="md"
-          startContent={<SearchIcon />}
-          type="search"
-        />
-        {
-          loading && <p>Cargando...</p>
-        }
-        {
-          suggestions.length > 0 && (
-            <ul>
-              {suggestions.map((movie) => (
-                <ul key={movie.id}
-                onClick={
-                  () => {
-                    router.push(`/${movie.id}`)
-                  }
-                }>
-                  {movie.title}
-                </ul>
-              ))}
-            </ul>
-          )
-        }
-      </NavbarContent>
+        >
+          <Card>
+            <CardBody className="overflow-visible py-2">
+              <Image
+                alt="Card background"
+                className="object-cover rounded-xl"
+                src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                width={270}
+              />
+            </CardBody>
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start mb-8">
+              <h4 className="font-bold text-large">{movie.title}</h4>
+            </CardHeader>
+          </Card>
+        </li>
+      ))}
+    </Card>
+  </div>
+)}
+
+          </div>
+        </NavbarContent>
         {/* <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem> */}
+            <Button as={Link} color="primary" href="#" variant="flat">
+              Sign Up
+            </Button>
+          </NavbarItem> */}
       </NavbarContent>
 
       <NavbarMenu>
         <NavbarMenuItem>
           <ul className="flex justify-center flex-col items-center">
-          <Link
-          className="text-gray-300 my-2 text-lg"
-          href="/topmovies">
-            Peliculas Populares
-          </Link>
-          <Link
-          className="text-gray-300 my-2 text-lg"
-          href="/mejorcalificado">
-            Mejor calificado
-          </Link>
-          <Link
-          className="text-gray-300 my-2 text-lg"
-          href="/proximo">
-            Próximo
-          </Link>
-          <Link
-          className="text-blue-600 my-2 text-lg"
-          href="/photos">
-            Fotos
-          </Link>
+            <Link className="text-gray-300 my-2 text-lg" href="/topmovies">
+              Películas Populares
+            </Link>
+            <Link className="text-gray-300 my-2 text-lg" href="/mejorcalificado">
+              Mejor calificado
+            </Link>
+            <Link className="text-gray-300 my-2 text-lg" href="/proximo">
+              Próximo
+            </Link>
+            <Link className="text-blue-600 my-2 text-lg" href="/photos">
+              Fotos
+            </Link>
           </ul>
         </NavbarMenuItem>
       </NavbarMenu>
